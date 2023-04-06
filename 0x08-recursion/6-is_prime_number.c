0dd11d20e4a3903487f2ab0a69b60d54f47417a8
DinduOffiah <99076158+DinduOffiah@users.noreply.github.com>
2023-04-06 15:53:08 +0100

    C - Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0.
+ * @n: value for assessment.
+ * @i: iterator
+ * real_prime - calculates if a number is normally a prime.
+ * Return: 1 if n is a prime number, else return 0.
+ */
+
+int real_prime(int n, int i);
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
