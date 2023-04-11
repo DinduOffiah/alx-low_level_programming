4089c5d81781b21ad41a7d5748fb9e7cc2825211
DinduOffiah <99076158+DinduOffiah@users.noreply.github.com>
2023-04-11 18:01:16 +0100

    C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@ char *str_concat(char *s1, char *s2)
 	int f, s;
 
 	if (s1 == NULL)
-        {
-          s1 = "";
-        }
+          {
+            s1 = "";
+          }
 
 	if (s2 == NULL)
         { 
