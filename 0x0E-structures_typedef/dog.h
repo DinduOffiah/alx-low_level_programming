577a8de6b3b3c2c93c86673d5733fecf294020bf
DinduOffiah <99076158+DinduOffiah@users.noreply.github.com>
2023-04-18 16:06:59 +0100

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - a structure "dog".
  * @name: name of the dog.
  * @age: age of the dog.
- * @owner: owner of the dog.
+ * @owner:  owner of the dog.
  * Description: This structure represents a dog.
  */
-
-struct dog 
+struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
