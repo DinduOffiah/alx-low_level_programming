9683883a48b5bafecfcd08ad9ea0a93801f700b9
DinduOffiah <99076158+DinduOffiah@users.noreply.github.com>
2023-04-30 22:14:12 +0100

    0x0A. C - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,29 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-    int sum = 0;
+	int num, digit, sum = 0;
 
-    if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 
-    for (int i = 1; i < argc; i++)
-    {
-        char *num = argv[i];
+		sum += atoi(argv[num]);
+	}
 
-        for (int j = 0; num[j] != '\0'; j++)
-        {
-            if (num[j] < '0' || num[j] > '9')
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
+	printf("%d\n", sum);
 
-        sum += atoi(num);
-    }
+	return (0);
+}
 
     printf("%d\n", sum);
     return (0);
